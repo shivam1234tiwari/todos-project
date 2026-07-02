a35df60cc2abc586ea2dbfc5b7f08eaf1c37106a
Rahul Tiwari <t.rahul3105@gmail.com>
2026-07-02 15:15:28 +0530

    register apis

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,38 +1,55 @@
-import jwt from 'jsonwebtoken'
-import User from '../models/User.js'
-import bcrypt from 'bcrypt'
-export const register=async(req,res)=>{
-    try{
-const {username,email,password,role,address}=req.body;
-if(!username || !email ||!password || !role ||! address){
-    res.status(400).json({message:"All Fields are Required"});
-}
-    }catch(error){}
-}
-export const login=async(req,res)=>{
-    try{
-
-    }catch(error){}
-}
-export const singleuser=async(req,res)=>{
-    try{
-
-    }catch(error){
-
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+export const register = async (req, res) => {
+  try {
+    const { username, email, password, role, address } = req.body;
+    if (!username || !email || !password || !role || !address) {
+      return res.status(400).json({ message: "All Fields are Required" });
     }
-}
-export const allUser=async(req,res)=>{
-    try{
-
-    }catch(error){}
-}
-export const updateUser=async(req,res)=>{
-    try{
-
-    }catch(error){}
-}
-export const deleteUser=async(req,res)=>{
-    try{
-
-    }catch(error){}
-}
+    const existuser = await User.findOne({ email });
+    if (existuser) {
+      return res.status(400).json({
+        message: "Email already registered please login"
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+      role,
+      address,
+    });
+    await user.save();
+    return res.status(200).json({
+      message: "User Registered",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+export const login = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+export const singleuser = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+export const allUser = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+export const updateUser = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+export const deleteUser = async (req, res) => {
+  try {
+  } catch (error) {}
+};
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,9 +1,21 @@
-import express from 'express'
-const route =express.Router();
+import express from "express";
 
-route.get('/todo',);
-route.post('/todo',);
-route.put('/todo/:id',);
-route.delete('/todo/:id')
+const route = express.Router();
+
+route.get("/todo", (req, res) => {
+    res.send("Get Todos");
+});
+
+route.post("/todo", (req, res) => {
+    res.send("Create Todo");
+});
+
+route.put("/todo/:id", (req, res) => {
+    res.send("Update Todo");
+});
+
+route.delete("/todo/:id", (req, res) => {
+    res.send("Delete Todo");
+});
 
 export default route;
