a5e9526b4daf97b823476b13fdf8be5f2816c8d6
Rahul Tiwari <t.rahul3105@gmail.com>
2026-07-04 15:51:10 +0530

    update delete profile api

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 export const register = async (req, res) => {
   try {
     const { username, email, password, role, address } = req.body;
@@ -11,7 +11,7 @@ export const register = async (req, res) => {
     const existuser = await User.findOne({ email });
     if (existuser) {
       return res.status(400).json({
-        message: "Email already registered please login"
+        message: "Email already registered please login",
       });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -36,93 +36,127 @@ export const register = async (req, res) => {
 };
 export const login = async (req, res) => {
   try {
-    const {email ,password}=req.body;
-    if(!email || !password){
+    const { email, password } = req.body;
+    if (!email || !password) {
       return res.status(400).json({
-        message:"All Fields are required"
-      })
+        message: "All Fields are required",
+      });
     }
-    const alreadyuser=await User.findOne({email});
-    if(alreadyuser){
-     return res.status(403).json({
-        message:"Already Email registered please login.."
-      })
+    const alreadyuser = await User.findOne({ email });
+    if (alreadyuser) {
+      return res.status(403).json({
+        message: "Already Email registered please login..",
+      });
     }
-    const isMatch=await bcrypt.compare(password,user.password);
-    if(!isMatch){
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
       return res.status(400).json({
-        message:"Password doesn 't matched"
-      })
+        message: "Password doesn 't matched",
+      });
     }
-    const token=jwt.sign({id:user._id,email:user.email},process.env.JWT_SECRET,{expiresIn:'1d'});
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" },
+    );
     return res.status(200).json({
-            message: "Login successful",
-            token,
-            user: {
-                id: user._id,
-                username: user.username,
-                email: user.email,
-                role: user.role
-            }
-        });
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+      },
+    });
   } catch (error) {
     return res.status(500).json({
-            message: error.message
-        });
+      message: error.message,
+    });
   }
 };
 export const singleuser = async (req, res) => {
   try {
-   
+    const OneUser = await User.findOne({ email });
+    if (!OneUser) {
+      return res.status(400).json({
+        sucess: false,
+        message: "User not found.",
+      });
+    }
+    return res.status(200).json({
+      sucess: true,
+      message: "User Found",
+      user: OneUser,
+    });
   } catch (error) {
     return res.status(500).json({
-            message: error.message
-        });
+      message: error.message,
+    });
   }
 };
 export const allUser = async (req, res) => {
   try {
-    const Users=await User.find();
-    if(!Users){
+    const Users = await User.find();
+    if (!Users) {
       res.status(400).json({
-        message:"Not User Found",
-      })
+        message: "Not User Found",
+      });
     }
     return res.status(200).json({
-      success:true,
-      message:"All Users found",
-      user:Users
-    })
+      success: true,
+      message: "All Users found",
+      user: Users,
+    });
   } catch (error) {
     return res.status(500).json({
-            message: error.message
-        });
+      message: error.message,
+    });
   }
 };
 export const updateUser = async (req, res) => {
   try {
-     const{id}=req.params;
-    const updatedata=await User.findByIdAndUpdate(
-      id.
-      req.body,
-      {new:true},
-      user
+    const { id } = req.params;
+    const updatedata = await User.findByIdAndUpdate(
+      id.req.body,
+      { new: true },
+      user,
     );
     return res.status(200).json({
-            message: "User updated successfully",
-            user: updatedata
-        });
+      message: "User updated successfully",
+      user: updatedata,
+    });
   } catch (error) {
     return res.status(500).json({
-            message: error.message
-        });
+      message: error.message,
+    });
   }
 };
 export const deleteUser = async (req, res) => {
   try {
+    const { id } = req.params;
+    const deleteUser = await User.findByIdAndDelete(id, req.body);
+    return res.status(200).json({
+      message: "User delete successfully",
+      user: deleteUser,
+    });
   } catch (error) {
     return res.status(500).json({
-            message: error.message
-        });
+      message: error.message,
+    });
+  }
+};
+export const profile = async (req, res) => {
+  try {
+    const userdata = await User.findById(req.user.id);
+    if (!userdata) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
   }
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { register,login,singleuser,allUser,updateUser,deleteUser } from '../controllers/authController.js';
+import { register,login,singleuser,profile,allUser,updateUser,deleteUser } from '../controllers/authController.js';
 const route=express.Router();
 
 route.get('/users',allUser);
 route.get('/users/:id',singleuser);
 route.post('/users/register',register);
+route.get("/profile", authMiddleware, profile);
 route.put('/users/:id',updateUser);
 route.delete('/users/:id',deleteUser);
 
