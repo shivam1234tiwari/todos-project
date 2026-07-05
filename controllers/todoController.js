4e9b09ac0d65826b217f3be9288c85bc509a909f
Rahul Tiwari <t.rahul3105@gmail.com>
2026-07-05 15:49:25 +0530

    delete api

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,105 +1,107 @@
-import Todo from '../models/Todo.js'
-export const newTodo=async(req,res)=>{
-    try{
-        const {title,description}=req.body;
-        const existtodo=await Todo.findOne({title});
-        if(title){
-            return res.status(200).json({
-                success:false,
-                message:"title already used"
-            })
-            const newtodo=await Todo.create({
-                title,
-                description
-            })
-            return res.status(201).json({
-                success:true,
-                message:"New todo created"
-            })
-        }
-    }catch(error){
-        return res.status(500).json({
-             success: false,
-            message: error.message
-        })
+import Todo from "../models/Todo.js";
+export const newTodo = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const existtodo = await Todo.findOne({ title });
+    if (existtodo) {
+      return res.status(400).json({
+        success: false,
+        message: "Title already exists",
+      });
     }
-}
-export const alltodos=async(req,res)=>{
-     try{
-        const gettodo=await Todo.find();
-        if(!gettodo){
-            return res.status(402).json({
-                success:false,
-                message:"todo not found"
-            })
-            return res.status(200).json({
-                success:true,
-                message:"Todo is found",
-                gettodo
-            })
-        }
-    }catch(error){
-         return res.status(500).json({
-             success: false,
-            message: error.message
-        })
-    }
-}
-export const onetodo=async(req,res)=>{
-    try{
-        const Singletodo=await Todo.findOne(req.body.id);
-        if(!Singletodo){
-            return res.status(402).json({
-                success:false,
-                message:"Not Found"
-            })
-        }
-        return res.status(200).json({
-            success:true,
-            message:"Todo Found"
-        })
-    }catch(error){
-         return res.status(500).json({
-             success: false,
-            message: error.message
-        })
-    }
-}
 
-export const edittodo=async(req,res)=>{
-     try{
-        const {id}=req.params;
-        const updatetodo=await Todo.findByIdAndUpdate(
-            id,
-            req.body,
-            {new:true}
-        )
-        return res.status(200).json({
-            success:true,
-            message:"Updated todo"
-        })
-    }catch(error){
-         return res.status(500).json({
-             success: false,
-            message: error.message
-        })
+    const newtodo = await Todo.create({
+      title,
+      description,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "New todo created",
+      todo: newtodo,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+export const alltodos = async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    return res.status(200).json({
+      success: true,
+      message: "Todos fetched successfully",
+      count: todos.length,
+      todos,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+export const onetodo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const Singletodo = await Todo.findOne(id);
+    if (!Singletodo) {
+      return res.status(402).json({
+        success: false,
+        message: "Not Found",
+      });
     }
-}
-export const deleteTodos=async(req,res)=>{
-     try{
-        const {id}=req.params;
-        const removetodos=await Todo.findByIdAndDelete(
-            id,
-            req.body
-        )
-        return res.status(200).json({
-            success:true,
-            message:"todo Delete successfull"
-        })
-    }catch(error){
-         return res.status(500).json({
-             success: false,
-            message: error.message
-        })
+    return res.status(200).json({
+      success: true,
+      message: "Todo Found",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const edittodo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatetodo = await Todo.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    return res.status(200).json({
+      success: true,
+      message: "Updated todo",
+      updatetodo,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+export const deleteTodos = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const removetodos = await Todo.findByIdAndDelete(id,);
+    if (!removetodo) {
+      return res.status(404).json({
+        success: false,
+        message: "Todo not found",
+      });
     }
-}
+    return res.status(200).json({
+      success: true,
+      message: "todo Delete successfull",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
